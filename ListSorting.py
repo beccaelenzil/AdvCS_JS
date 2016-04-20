a65fee3a692e1c138f165380b3dfecf4f3c00e77
FluffyLincoln <juliesmith700@gmail.com>
2016-04-20 14:06:25 -0700

    Worked on counting number of iterations

--- ListSorting.py
+++ ListSorting.py
@@ -1,4 +1,11 @@
 import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
+from random import shuffle
+
+def create_random_list(length):
+    some_list = [i for i in range(length)]
+    shuffle(some_list)
+    return some_list
 
 plt.ion() #interactive
 
@@ -9,47 +16,64 @@ def display(some_list):
     plt.draw()
 
 def bubbleSort(list):
-    k = len(list)-1
+    counter = 0
     flip = True
-    #y = [i for i in range(len(list)+1)]
-    #x = [i for i in range(len(y))]
-    while flip == True:
+    k = len(list)-1
+    while flip:
         flip = False
         for i in range(k):
-            plt.show()
+            counter += 1
             if list[i] > list[i+1]:
-                list[i+1], list[i]= list[i], list[i+1]
+                list[i], list[i+1]= list[i+1], list[i]
                 flip = True
-                display(list)
-                print(list)
+                #display(list)
 
-
-        plt.show()
         k -= 1
-    return list
+    return counter
 
-bubbleSort([10, 4, 3, 1])
+#print(bubbleSort([10, 4, 3, 1]))
 
+'''
+num_iter = []
+for i in range(100):
+    print(i)
+    n = bubbleSort(create_random_list(100))
+    num_iter.append(n)
+
+ave_iter = sum(num_iter)/len(num_iter)
+print(ave_iter)
+'''
 
 def SelectionSort(list):
+    counter = 0
     for d in range(len(list)):
         min_value = d
 
         for i in range(d+1, len(list)):
-            print(list)
+            #print(list)
+            counter += 1
             if list[i] < list[min_value]: #if the number in the list is less than the current min
                 min_value = i #that list number is new min value
         list[min_value], list[d] = list[d], list[min_value]
 
-    return list
+    return counter
 
 #print(SelectionSort([3, 2, 1, 10, 4]))
+#print(counter)
+'''
+num_iter1 = []
+for i in range(100):
+    print(i)
+    q = SelectionSort(create_random_list(100))
+    num_iter1.append(q)
 
+ave_iter1 = sum(num_iter1)/len(num_iter1)
+print(ave_iter1)
+'''
 
-
-
-
-
+def insertionSort(list):
+    for i in range(len(list)):
+        
 
 
 
